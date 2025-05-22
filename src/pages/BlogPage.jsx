0229b754892f0678cc4c0473ba26c9b11643de4f
alfredo.admin <alfredohoyosrioja@outlook.com>
2025-05-22 10:58:42 -0500

    Sitio en React con enlace

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,6 +1,27 @@
+import HeaderComponent from "../components/HeaderComponent"
+
 function BlogPage() {
+  const handlesubmit =(e)=>{
+    e.preventDefault();
+    console.log("El formulario se ha enviado");
+  }
   return (
+    <>
+    <HeaderComponent></HeaderComponent>
     <div>BlogPage</div>
+    <h1>FORMULARIO #01234</h1>
+    <form onSubmit={handlesubmit}>
+      <fieldset>
+      <label htmlFor="username ">Nombre</label>
+      <input type="text" id="username"/>
+      </fieldset>
+      <fieldset>
+      <label htmlFor="comentario">Comentario</label>
+      <input type="text" id="comentario" />
+      </fieldset>
+      <button>Enviar</button>
+    </form>
+    </>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import HeaderComponent from "../components/HeaderComponent"
 
 function HomePage() {
+  const saySomething = ()=> {
+    console.log("Di Algo");
+    alert("Ahora");
+  }
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <div>HomePage</div>
+      <hr />
+      <button onClick={saySomething}>Pulsa Click</button>
     </>
     
   )
